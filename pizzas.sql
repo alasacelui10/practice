18ec7443ed5578aafd6947ddd44154c5d149f07e
Ace Lui Manalo <65894286+alasacelui10@users.noreply.github.com>
2020-11-03 00:24:42 +0800

    Add files via upload

---
+++ pizzas.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2020 at 05:23 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ninja_pizza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pizzas`
+--
+
+CREATE TABLE `pizzas` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `ingredients` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pizzas`
+--
+
+INSERT INTO `pizzas` (`id`, `title`, `ingredients`, `email`, `created_at`) VALUES
+(9, 'Alas Pizza', 'tomato , cheese, mushrooms', 'alasacelui010@gmail.com', '2020-11-01 05:50:41'),
+(13, 'Alas Pizza', 'tomato , cheese, mushrooms', 'alasacelui010@gmail.com', '2020-11-01 05:57:37'),
+(14, 'Ace Pizza Express', 'tomato , cheese, mushrooms , potato , ketchupsss', 'alasacelui010@gmail.coms', '2020-11-01 05:58:02');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pizzas`
+--
+ALTER TABLE `pizzas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pizzas`
+--
+ALTER TABLE `pizzas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
